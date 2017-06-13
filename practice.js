dca18591443226f758586e2fe3c9710e9573b1dd
Aaron Kim <aaronkim1205@gmail.com>
2017-06-12 18:09:48 -0700

    Toggle complete

--- practice.js
+++ practice.js
@@ -1,44 +1,39 @@
-var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
-
-function displayTodos() {
-  console.log('These are my todos:', todos);
-};
-
-function addTodo(item1) {
-  todos.push(item1);
-  displayTodos();
-};
-
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-};
-
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
-
-deleteTodo(0);
-
-
-var aaron = {
-  name: 'Aaron',
-  sayName: function() {
-    console.log(this.name);
-  }
-};
-
-
-var itemList = {
-  items: ['item 1', 'item 2', 'item 3'],
-  displayItems: function(){
-    console.log('my items:', this.items);
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log('your todos is empty');
+    } else {
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
   },
-  addItem: function(newItem) {
-    this.items.push(newItem);
-    this.displayItems();
+  addTodos: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
   }
 };
 
-itemList.addItem('newwwwww item');
+todoList.displayTodos();
